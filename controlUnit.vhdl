4208b8ea98c852c8c80e03d27391be023ca87655
Matt <matt.bennett4478@gmail.com>
2015-11-06 22:54:42 -0600

    this is a commit

--- controlUnit.vhdl
+++ controlUnit.vhdl
@@ -38,60 +38,84 @@ ARCHITECTURE behavior OF ControlUnit IS
                
        signal wmfc: std_logic;
        shared variable stage: integer:= 0;
-
 BEGIN
    
        PROCESS(clock, reset)BEGIN
    
        IF(rising_edge(clock))THEN
            IF(reset = '1')THEN
-           stage := 0;
+				stage := 0;
            END IF;
            IF((mfc = '1' or wmfc = '0'))THEN
-               stage := stage mod 5+1;
+				stage := stage mod 5+1;
            end if;
            if(stage = 1)then            
-           wmfc <= '1';
-           alu_op <= "00";
-           c_select <= "01";
-           y_select <= "00";
-           rf_write <= '0';
-           b_select <= '0';
-           a_inv <= '0';
-           b_inv <='0';
-           extend <= "00";
-           ir_enable <= '0';
-           ma_select <= '1';
-           mem_read <= '1';
-           mem_write <= '0';
-           pc_select <= '1';
-           pc_enable <= mfc;
-           inc_select <= '0';
-           
+				wmfc <= '1';
+				alu_op <= "00";
+				c_select <= "01";
+				y_select <= "00";
+				rf_write <= '0';
+				b_select <= '0';
+				a_inv <= '0';
+				b_inv <='0';
+				extend <= "00";
+				ir_enable <= '1';
+				ma_select <= '1';
+				mem_read <= '1';
+				mem_write <= '0';
+				pc_select <= '1';
+				pc_enable <= mfc;
+				inc_select <= '0';
            elsif(stage = 2)then
                wmfc <= '0';
                ir_enable <= '0';
                mem_read <= '0';
                pc_enable <= '0';
+			  ELSIF(stage = 3)THEN
+				IF((opCode(3) = '0' AND opCode(2) = '0'))THEN
+               IF((opCode(1) = '0') AND (opCode(0) = '1'))THEN
+					ELSIF(( opCode(1) = '0' AND opCode (0) = '0'))THEN
+							IF(opx = "111" ) THEN
+								alu_op <= "00";
+							ELSIF( opx = "110") THEN
+								alu_op <= "01";
+							ELSIF( opx = "101") then
+								alu_op <= "10";
+							ELSIF( opx = "100") then
+								alu_op <= "11";
+							ELSIF( opx = "011") then
+								alu_op <= "11";
+								b_inv <= '1';
+							end if;
+               end if;
+				end if;
            ELSIF(stage = 3)THEN
            IF((opCode(3) = '0' AND opCode(2) = '0'))THEN
-               IF((opCode(1) = '0') AND (opCode(0) = '1'))THEN
-               ELSIF(( opCode(1) = '0' AND opCode (0) = '0'))THEN
-               IF(opx = "111" ) THEN
-               alu_op <= "00";
-               ELSIF( opx = "110") THEN
-               alu_op <= "00";
-               ELSIF( opx = "101") then
-               alu_op <= "00";
-               ELSIF( opx = "100") then
-               alu_op <= "00";
-               ELSIF( opx = "011") then
-               alu_op <= "00";
-               b_inv <= '1';
-               end if;
-               end if;
+						IF((opCode(1) = '0') AND (opCode(0) = '1'))THEN
+						ELSIF(( opCode(1) = '0' AND opCode (0) = '0'))THEN
+							IF(opx = "111" ) THEN
+							alu_op <= "00";
+							ELSIF( opx = "110") THEN
+							alu_op <= "00";
+							ELSIF( opx = "101") then
+							alu_op <= "00";
+							ELSIF( opx = "100") then
+							alu_op <= "00";
+							ELSIF( opx = "011") then
+							alu_op <= "00";
+							b_inv <= '1';
+							end if;
+						end if;
 					end if;
-					end if;
-end if;
-end PROCESS;
+			  elsif(stage = 4)then
+					
+			  elsif(stage = 5)then
+					rf_write <= '1';
+					
+				end if;
+			end if;
+	end process;
+	
+--end if;
+--end PROCESS;
 end behavior;
